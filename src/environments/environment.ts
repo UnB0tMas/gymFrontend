94503ef5f43cd89ec53f30cfbd4596b41aafad70
UnB0tMas <tresdynx@gmail.com>
2025-07-09 12:19:49 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiMs1: 'https://ms1-u7g6.onrender.com/api',
   apiMs2: 'https://ms2-rqe2.onrender.com/api',
-  apiMs3: 'https://ms3-97ze.onrender.com'
+  apiMs3: 'https://ms3-97ze.onrender.com/api'
 };
