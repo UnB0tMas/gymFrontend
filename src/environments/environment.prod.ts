5b1600f2c6cb9aabe942a7d139dda1ce9211c92c
UnB0tMas <tresdynx@gmail.com>
2025-07-09 10:18:44 -0500

    Primer commit a gymFrontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiMs1: 'https://ms1-u7g6.onrender.com/api',
-  apiMs2: 'https://ms2-rqe2.onrender.com/api',
-  apiMs3: 'https://ms3-3enf.onrender.com/api'
+  apiMs1: '/api',
+  apiMs2: '/ms2',
+  apiMs3: '/ms3'
 };
